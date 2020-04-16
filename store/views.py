2b3be29bc30cf3494c9d9b3f1b77c2aca9da127e
Saurav <sauravsrivastav103@gmail.com>
2020-04-17 00:06:51 +0530

    basic templating done

--- store/views.py
+++ store/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Saurav Shrivastav',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 15 2020'
+    },
+    {
+        'author': 'Vaibhav Shrivastav',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 16 2020'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>home</h1>')
+    context = {
+        'posts': posts
+    }
+
+    return render(request, 'store/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>about</h1>')
+    return render(request, 'store/about.html', { 'title': 'About'})
