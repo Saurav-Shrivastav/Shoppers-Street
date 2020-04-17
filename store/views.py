093edd1ae31aae8f61159a41a974c86add041180
Saurav <sauravsrivastav103@gmail.com>
2020-04-18 00:09:22 +0530

    Database for the posts was successfully created

--- store/views.py
+++ store/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Saurav Shrivastav',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 15 2020'
-    },
-    {
-        'author': 'Vaibhav Shrivastav',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 16 2020'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
 
     return render(request, 'store/home.html', context)
