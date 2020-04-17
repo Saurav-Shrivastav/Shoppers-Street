f1bde408938c23a9bb5631e552fbd70e5b8c7265
Saurav <sauravsrivastav103@gmail.com>
2020-04-18 00:35:28 +0530

    Updated the model and also started the users app

--- store/models.py
+++ store/models.py
@@ -8,6 +8,7 @@ class Post(models.Model):
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    link = models.URLField(max_length = 200, default = 'www.google.com')
 
     def __str__(self):
         return self.title
